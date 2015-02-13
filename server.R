8ab2f0e715a0520082ae683b0c735da1f4a97365
AmritPatel <adp013@gmail.com>
2015-02-13 12:12:48 -0500

    Update Shiny app files

--- server.R
+++ server.R
@@ -36,7 +36,7 @@ winning <- function(team, opp) {
   recentPerf <-
     bpi %>% 
     mutate(recentPerf=(BPI-mean(BPI))/mean(BPI)*100) %>%
-    filter(DATE==as.character(Sys.Date())) %>%
+    filter(DATE==as.character(bpi[nrow(bpi),1])) %>%
     group_by() %>%
     arrange(desc(recentPerf))
   
@@ -50,20 +50,20 @@ winning <- function(team, opp) {
   # Get corresponding BPI data
   
   tESPN     <- dfTeams[dfTeams$Yahoo==team, 1]; tESPN <- recentPerf[recentPerf$TEAM==tESPN, ]
-  tBPI      <- as.numeric(tESPN[grep(Sys.Date(), tESPN$DATE), 4])
-  tBPI      <- if (length(tBPI) == 0L) 0 else as.numeric(tESPN[grep(Sys.Date(), tESPN$DATE), 4])
+  tBPI      <- as.numeric(tESPN[grep(as.character(bpi[nrow(bpi),1]), tESPN$DATE), 4])
+  tBPI      <- if (length(tBPI) == 0L) 0 else as.numeric(tESPN[grep(as.character(bpi[nrow(bpi),1]), tESPN$DATE), 4])
   oESPN     <- dfTeams[dfTeams$Yahoo==opp, 1];  oESPN <- recentPerf[recentPerf$TEAM==oESPN, ] 
-  oBPI      <- as.numeric(oESPN[grep(Sys.Date(), oESPN$DATE), 4])
-  oBPI      <- if (length(oBPI) == 0L) 0 else as.numeric(oESPN[grep(Sys.Date(), oESPN$DATE), 4])
+  oBPI      <- as.numeric(oESPN[grep(as.character(bpi[nrow(bpi),1]), oESPN$DATE), 4])
+  oBPI      <- if (length(oBPI) == 0L) 0 else as.numeric(oESPN[grep(as.character(bpi[nrow(bpi),1]), oESPN$DATE), 4])
   
   # Get corresponding recent performance data
   
   pretRP    <- dfTeams[dfTeams$Yahoo==team, 1]; pretRP <- recentPerf[recentPerf$TEAM==pretRP, ]
-  tRP       <- as.numeric(pretRP[grep(Sys.Date(), pretRP), 5])
-  tRP       <- if (length(tRP) == 0L) 0 else as.numeric(pretRP[grep(Sys.Date(), pretRP), 5])
+  tRP       <- as.numeric(pretRP[grep(as.character(bpi[nrow(bpi),1]), pretRP), 5])
+  tRP       <- if (length(tRP) == 0L) 0 else as.numeric(pretRP[grep(as.character(bpi[nrow(bpi),1]), pretRP), 5])
   preoRP    <- dfTeams[dfTeams$Yahoo==opp, 1]; preoRP <- recentPerf[recentPerf$TEAM==preoRP, ]
-  oRP       <- as.numeric(preoRP[grep(Sys.Date(), preoRP), 5])
-  oRP       <- if (length(oRP) == 0L) 0 else as.numeric(preoRP[grep(Sys.Date(), preoRP), 5]) 
+  oRP       <- as.numeric(preoRP[grep(as.character(bpi[nrow(bpi),1]), preoRP), 5])
+  oRP       <- if (length(oRP) == 0L) 0 else as.numeric(preoRP[grep(as.character(bpi[nrow(bpi),1]), preoRP), 5]) 
   
   newdata <- data.frame(tBPI=tBPI, tRP=tRP, oBPI=oBPI, oRP=oRP)
   
